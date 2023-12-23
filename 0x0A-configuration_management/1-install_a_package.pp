a703a13615fbfc12367202d50079f1c5dcc5babd
othmanoach <othmanoach@gmail.com>
2023-12-22 17:06:27 -0800

    1

--- 0x0A-configuration_management/1-install_a_package.pp
+++ 0x0A-configuration_management/1-install_a_package.pp
@@ -1,5 +1,5 @@
-exec { 'install_flask':
-  command => '/usr/bin/pip3 install Flask==2.1.0',
-  path    => '/usr/local/sbin:/usr/local/bin:/usr/sbin:/usr/bin:/sbin:/bin',
-  unless  => '/usr/bin/pip3 show Flask | grep -q "Version: 2.1.0"',
+# install puppet-lint -v 2.5.0
+
+exec { 'puppet-lint':
+  command => '/usr/bin/apt-get -y install puppet-lint -v 2.5.0',
 }
