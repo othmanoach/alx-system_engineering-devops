c2fedf52ded6f4515da8d0023dce21fa2c1befe9
othmanoach <othmanoach@gmail.com>
2023-12-22 16:59:03 -0800

    e

--- 0x0A-configuration_management/1-install_a_package.pp
+++ 0x0A-configuration_management/1-install_a_package.pp
@@ -1,7 +1,5 @@
-#install flask
-class Flask {
-  package { 'Flask':
-    ensure => '2.1.0',
-    provider => 'pip3',
-  }
+exec { 'install_flask':
+  command => '/usr/bin/pip3 install Flask==2.1.0',
+  path    => '/usr/local/sbin:/usr/local/bin:/usr/sbin:/usr/bin:/sbin:/bin',
+  unless  => '/usr/bin/pip3 show Flask | grep -q "Version: 2.1.0"',
 }
