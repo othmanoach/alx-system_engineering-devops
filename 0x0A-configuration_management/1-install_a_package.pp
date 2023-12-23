e00e8a5968270c054bbffc2251a1c02a3efa9977
othmanoach <133314105+othmanoach@users.noreply.github.com>
2023-12-24 00:00:55 +0100

    Update 1-install_a_package.pp

--- 0x0A-configuration_management/1-install_a_package.pp
+++ 0x0A-configuration_management/1-install_a_package.pp
@@ -1,11 +1,19 @@
-#Install flask version 2.1.0
+# 1-install_a_package.pp
+# Install Flask version 2.1.0 using pip3
 
-
-exec {'pip3 install flask':
-require => Exec['python-installed'],
-command => '/usr/bin/pip3 install flask==2.1.0'
+package { 'Flask':
+  ensure   => '2.1.0',
+  provider => 'pip3',
+  require  => Package['python3-pip'], # Ensure pip3 is installed first
 }
 
-exec {'python-installed':
-command => '/usr/bin/which python3'
+package { 'Werkzeug':
+  ensure   => '2.1.1',
+  provider => 'pip3',
+  require  => Package['python3-pip'], # Ensure pip3 is installed first
+}
+
+# Ensure python3-pip is installed
+package { 'python3-pip':
+  ensure => installed,
 }
