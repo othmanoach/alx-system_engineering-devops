d86d39436d19de6065ae74ca393f0588fbe70d1c
othmanoach <othmanoach@gmail.com>
2024-02-15 08:02:20 -0800

    subs

--- 0x16-api_advanced/0-subs.py
+++ 0x16-api_advanced/0-subs.py
@@ -1,21 +1,21 @@
 #!/usr/bin/python3
-""" Exporting csv files"""
-import json
-import requests
-import sys
+"""test"""
 
+import requests
 
 def number_of_subscribers(subreddit):
-    """Read reddit API and return number subscribers """
-    username = 'ledbag123'
-    password = 'Reddit72'
-    user_pass_dict = {'user': username, 'passwd': password, 'api_type': 'json'}
-    headers = {'user-agent': '/u/ledbag123 API Python for Holberton School'}
-    url = 'https://www.reddit.com/r/{}/about.json'.format(subreddit)
-    client = requests.session()
-    client.headers = headers
-    r = client.get(url, allow_redirects=False)
-    if r.status_code == 200:
-        return (r.json()["data"]["subscribers"])
+    url = f"https://www.reddit.com/r/{subreddit}/about.json"
+    headers = {"othmanoach": "othmano"}
+    response = requests.get(url, headers=headers)
+
+    # Check if the response is successful
+    if response.status_code == 200:
+        data = response.json()
+
+        # Check if the subreddit exists
+        if data["kind"] == "t5":
+            return data["data"]["subscribers"]
+        else:
+            return 0
     else:
-        return(0)
+        return 0
